9cdbb64eb433f79ef2f03736007a926dc450918d
Benjamin Crisman <bencrisman@bcsquared.tech>
2022-12-19 11:23:29 -0700

    add background video and 3d database object

--- Assets/Scripts/Models/AnimPanel.cs
+++ Assets/Scripts/Models/AnimPanel.cs
@@ -1,19 +1,15 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class AnimPanel : MonoBehaviour {
     public EventHandler<Dictionary<BarGraph, float>> GraphUpdated;
 
     [SerializeField] private ScorePanel _scorePanel;
-    [SerializeField] private Transform _graphsParent;
     [SerializeField] private Transform _dataStreamParent;
-    [SerializeField] private Transform _dataStreamDest;
     [SerializeField] private GameObject _dataStreamPrefab;
-
     [SerializeField] private DataStreamManager _dataStreamManager;
+    [SerializeField] private Video _video;
 
     private Dictionary<BarGraph, float> _graphsMap = new();
     private List<BarGraph> _graphs;
@@ -39,42 +35,15 @@ public class AnimPanel : MonoBehaviour {
 
         _dataStreamManager.Setup(_isAMD ? 1 : setup.AmdFinalDataValue / setup.CompFinalDataValue);
         _scorePanel.Setup(setup.ScoreLabel);
-        //InitGraphs(setup.AmdFinalDataValue / setup.CompFinalDataValue);
     }
 
     public void RunDemo() {
         if (_isRunning) return;
         _isRunning = true;
 
-        //foreach (var g in _graphs) {
-        //    g.RiseOverTime();
-        //}
-
+        _video.Run();
         _dataStreamManager.Run();
         _scorePanel.Run(_totalDurationSec, _finalScore);
-    }
-
-    private void InitGraphs(float delta) {
-        _graphs = _graphsParent.GetComponentsInChildren<BarGraph>().ToList();
-
-        var maxDuration = _data.Durations.Max();
-        var maxQPH = _data.QueriesPerHour.Max();
-        var min = Mathf.Min(_data.Durations.Count, _graphs.Count);
-
-        for (var i = 0; i < min; ++i) {
-            var q = _data.QueriesPerHour[i];
-            var t = _data.Durations[i];
-            var g = _graphs[i];
-            g.RawValueUpdated += (s, e) => OnGraphRawValueUpdated(g, e);
-            g.Setup(_isAMD, q, q / maxQPH, t / maxDuration * _totalDurationSec, delta, _dataStreamPrefab, _dataStreamParent, _dataStreamDest.position);
-        }
-    }
-
-    private void OnGraphRawValueUpdated(BarGraph g, float val) {
-        return;
-
-        _graphsMap[g] = val;
-        _scorePanel.UpdateScore(_graphsMap.Sum(x => x.Value));
     }
 
     private void OnScoreFinished(object sender, EventArgs e) {
---
+++ Assets/Scripts/Models/Video.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(VideoPlayer))]
+[RequireComponent(typeof(RawImage))]
+public class Video : MonoBehaviour {
+    private VideoPlayer _vp;
+    private RawImage _rawImg;
+
+    private void Awake() {
+        _vp = GetComponent<VideoPlayer>();
+        _rawImg = GetComponent<RawImage>();
+
+        Init();
+    }
+
+    private IEnumerator Start() {
+        _vp.Prepare();
+
+        while (!_vp.isPrepared)
+            yield return null;
+
+        _rawImg.texture = _vp.texture;
+        _vp.isLooping = true;
+    }
+
+    public void  Run() {
+        StartCoroutine(FadeIn());
+        _vp.Play();
+    }
+
+    private void Init() {
+        var col = _rawImg.color;
+        col.a = 0f;
+        _rawImg.color = col;
+    }
+
+    private IEnumerator FadeIn() {
+        var t = 0f;
+        var duration = 0.5f;
+        var begin = _rawImg.color;
+        var end = Color.white;
+
+        while (t < duration) {
+            _rawImg.color = Color.Lerp(begin, end, t / duration);
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        _rawImg.color = end;
+    }
+}
